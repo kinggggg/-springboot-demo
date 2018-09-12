b49fe5832a16908e09f4dd7e35676713c2591f34
zeek <liweibooooo@126.com>
2018-09-12 09:06:52 +0800

    项目初始化

---
+++ src/main/java/com/zeek/springboot/springbootdemo/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.zeek.springboot.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/tests")
+public class TestController {
+
+}
