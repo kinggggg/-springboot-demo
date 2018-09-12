4c13f2931004740ef673d10c9bd9692ddbffb659
zeek <liweibooooo@126.com>
2018-09-12 13:46:59 +0800

    增加测试接口；修改端口号

--- src/main/java/com/zeek/springboot/springbootdemo/controller/TestController.java
+++ src/main/java/com/zeek/springboot/springbootdemo/controller/TestController.java
@@ -1,11 +1,19 @@
 package com.zeek.springboot.springbootdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @RequestMapping("/tests")
 public class TestController {
 
+    @GetMapping
+    @ResponseBody
+    public Object hello() {
+
+        return "hello world" ;
+    }
 }
