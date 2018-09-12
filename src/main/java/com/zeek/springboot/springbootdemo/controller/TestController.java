343a41d4610b08a87bb3cbf66dbe70e39cf89346
zeek <liweibooooo@126.com>
2018-09-12 15:01:43 +0800

    使用spring集成redis存储session信息替代Tomcat session

--- src/main/java/com/zeek/springboot/springbootdemo/controller/TestController.java
+++ src/main/java/com/zeek/springboot/springbootdemo/controller/TestController.java
@@ -3,7 +3,12 @@ package com.zeek.springboot.springbootdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -15,5 +20,23 @@ public class TestController {
     public Object hello() {
 
         return "hello world" ;
+    }
+
+    @RequestMapping(value = "/first", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> firstResp (HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        request.getSession().setAttribute("request-Url", request.getRequestURL());
+        map.put("request-Url", request.getRequestURL());
+        return map;
+    }
+
+    @RequestMapping(value = "/sessions", method = RequestMethod.GET)
+    @ResponseBody
+    public Object sessions (HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        map.put("sessionId", request.getSession().getId());
+        map.put("message", request.getSession().getAttribute("request-Url"));
+        return map;
     }
 }
